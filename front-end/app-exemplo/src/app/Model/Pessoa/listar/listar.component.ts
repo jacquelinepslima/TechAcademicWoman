decb38ddcfcc44da60ea6434af3fedbf41364b6f
jacquelinepslima <jacquelinepslima@gmail.com>
2021-11-17 16:51:50 -0300

    Rotiamento dos botões

--- front-end/app-exemplo/src/app/Model/Pessoa/listar/listar.component.ts
+++ front-end/app-exemplo/src/app/Model/Pessoa/listar/listar.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar',
   templateUrl: './listar.component.html'
 })
-export class ListarComponent implements OnInit {
+export class ListarComponent {
 
-  constructor() { }
+  constructor(private router: Router){ }
 
-  ngOnInit(): void {
+  Listar(){
+    this.router.navigate(['listar']);
   }
 
 }
--- front-end/app-exemplo/src/app/app.component.ts
+++ front-end/app-exemplo/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exemplo de front e back';
+
+  constructor(){ }
 }
