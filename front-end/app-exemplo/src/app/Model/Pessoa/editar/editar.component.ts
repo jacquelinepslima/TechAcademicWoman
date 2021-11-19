8af3ad96cb08066f457fa8573012f3ac246c90bd
jacquelinepslima <jacquelinepslima@gmail.com>
2021-11-19 17:37:57 -0300

    Atualizacao dos arquivos editar e listar

--- front-end/app-exemplo/src/app/Model/Pessoa/editar/editar.component.ts
+++ front-end/app-exemplo/src/app/Model/Pessoa/editar/editar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicoService } from '../../../Service/servico.service';
+import { Pessoa } from '../../entidade/Pessoa';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar',
@@ -8,7 +10,7 @@ import { ServicoService } from '../../../Service/servico.service';
 export class EditarComponent implements OnInit {
 
   pessoa: Pessoa = new Pessoa();
-  constructor(private service: ServicoService) { }
+  constructor(private service: ServicoService, private router: Router) { }
 
   ngOnInit(): void {
     this.buscaPessoa();
@@ -18,10 +20,18 @@ export class EditarComponent implements OnInit {
     let idLocalStorage = localStorage.getItem("id");
     this.service.getPessoaById(idLocalStorage)
     .subscribe(data => {
-      this.pessoa = data;
+      this.pessoa =data;
     })
 
     console.log(this.pessoa);
+  }
+
+  atualizarPessoa(){
+    this.service.updatePessoa(this.pessoa)
+    .subscribe(data => {
+      this.pessoa = data;
+      this.router.navigate(['listar']);
+    })
   }
 
 }
