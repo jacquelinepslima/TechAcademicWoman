9bee4fd4c8f6baa4a1365c2009d666586fb77ade
jacquelinepslima <jacquelinepslima@gmail.com>
2021-11-19 14:53:01 -0300

    metodo buscarPorId usando o Optional

--- front-end/demo/src/main/java/com/example/demo/PessoaController.java
+++ front-end/demo/src/main/java/com/example/demo/PessoaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pessoa")
@@ -24,6 +25,15 @@ public class PessoaController {
     @PostMapping
     public Pessoa salvar(@RequestBody Pessoa p){
         return pessoaRepository.save(p);
+    }
+
+    @GetMapping("/{id}")
+    public Pessoa buscarPorId(@PathVariable Long id){
+        Optional<Pessoa> pessoaPesquisada = pessoaRepository.findById(id);
+        if (pessoaPesquisada.isPresent()){
+            return pessoaRepository.getById(id);
+        }
+        return null;
     }
 
 }
